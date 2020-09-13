055fdf94935d5a2bf75d8c930c321e588accc90f
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2020-09-13 16:39:45 +0530

    Add files via upload

---
+++ backend/student/makepdf1.php
@@ -0,0 +1,38 @@
+<?php
+    require_once __DIR__ . '/vendor/autoload.php';
+    if(isset($_POST["mkpdf1"])) {
+        $rollno = $_POST['rollno'];
+        $mobile = $_POST['mobile'];
+        $name = $_POST['name'];
+        $bg = $_POST['bg'];
+        $email = $_POST['email'];
+        $birthdate = $_POST['birthdate'];
+        $monthaddm = $_POST['myad'];
+        $hobbies = $_POST['hobbies'];
+        $sports = $_POST['sports'];
+        $health = $_POST['health'];
+        $address = $_POST['address'];
+    
+        //create new pdf instance
+        $mpdf = new \Mpdf\Mpdf();
+
+        $data = '';
+
+        $data .= '<h1>Student Details Form</h1><br>';
+        $data .= '<strong>Name : </strong>'.$name.'<br><br>';
+        $data .= '<strong>ID Card No : </strong>'.$rollno.'<br><br>';
+        $data .= '<strong>Mobile No : </strong>'.$mobile.'<br><br>';
+        $data .= '<strong>Address : </strong>'.$address.'<br><br>';
+        $data .= '<strong>Blood Group : </strong>'.$bg.'<br><br>';
+        $data .= '<strong>Email : </strong>'.$email.'<br><br>';
+        $data .= '<strong>Birthdate : </strong>'.$birthdate.'<br><br>';
+        $data .= '<strong>Date Of Admission : </strong>'.$monthaddm.'<br><br>';
+        $data .= '<strong>Hobbies : </strong>'.$hobbies.'<br><br>';
+        $data .= '<strong>Sports : </strong>'.$sports.'<br><br>';
+        $data .= '<strong>Health Problems : </strong>'.$health.'<br><br>';
+
+        $mpdf->WriteHTML($data);
+        $mpdf->Output('studentdetails.pdf', 'D');
+    
+    }
+?>

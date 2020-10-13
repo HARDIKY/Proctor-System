2beffb30667c1000bdb2f1b0cc5eeef85a13cbbe
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2020-10-13 23:30:36 +0530

    Oct-13's Work DONE

--- backend/Oct-13.sql
+++ backend/Oct-13.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 10, 2020 at 04:29 PM
+-- Generation Time: Oct 13, 2020 at 05:58 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -80,28 +80,8 @@ CREATE TABLE IF NOT EXISTS `familiydetail` (
 
 INSERT INTO `familiydetail` (`std_rollno`, `f_fname`, `f_mname`, `f_lname`, `f_email`, `f_mob`, `f_age`, `f_qua`, `f_occ`, `m_fname`, `m_mname`, `m_lname`, `m_email`, `m_mob`, `m_age`, `m_qua`, `m_occ`, `s1_fname`, `s1_mname`, `s1_lname`, `s1_email`, `s1_mob`, `s1_age`, `s1_qua`, `s1_occ`, `s2_fname`, `s2_mname`, `s2_lname`, `s2_email`, `s2_mob`, `s2_age`, `s2_qua`, `s2_occ`, `s3_fname`, `s3_mname`, `s3_lname`, `s3_email`, `s3_mob`, `s3_age`, `s3_qua`, `s3_occ`) VALUES
 ('1', 'abc', 'abc', 'abc', 'a@gmail.com', '', '50', 'knkn', 'ss', 'knkn', 'knknnn', 'knkn', 'a@gmail.com', '1234567896', '10', '1234567896', 'ak@gmail.com', 'a', 'a', 'a', 'a@gmail.com', 'a', '10', 'a', 'a', 'a', 'a', 'a', 'a@gmail.com', 'a', '10', 'a', 'a', 'b', 'b', 'b', 'a@gmail.com', 'b', '10', 'cccccccc', 'cccccc'),
-('2', 'xyz', 'xyz', 'xyz', 'xyz@gmail.com', '', '50', 'xyz', 'xyz', 'abc', 'abc', 'abc', 'abc@gmail.com', '232323', '48', 'abc', 'abc', 'asd', 'asd', 'asd', 'asd@gmail.com', '454545', '25', 'asd', 'asd', 'aaaa', 'aaaaa', 'aaaaa', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-('', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `files`
---
-
-DROP TABLE IF EXISTS `files`;
-CREATE TABLE IF NOT EXISTS `files` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
-  `pdf_file` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `pdf_file`) VALUES
-(1, 'IP Assignment 01.pdf');
+('10', 'abc', 'abc', 'abc', 'abc@gmail.com', '', '50', 'abc', 'abc', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'abc', 'abc', 'abc', '', '', '12', '', '', '', '', '', '', '', '', '', ''),
+('2', 'xyz', 'xyz', 'xyz', 'xyz@gmail.com', '', '50', 'xyz', 'xyz', 'abc', 'abc', 'abc', 'abc@gmail.com', '232323', '48', 'abc', 'abc', 'asd', 'asd', 'asd', 'asd@gmail.com', '454545', '25', 'asd', 'asd', 'aaaa', 'aaaaa', 'aaaaa', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -111,7 +91,7 @@ INSERT INTO `files` (`id`, `pdf_file`) VALUES
 
 DROP TABLE IF EXISTS `futureplans`;
 CREATE TABLE IF NOT EXISTS `futureplans` (
-  `std_rollno` varchar(100) NOT NULL,
+  `rollno` varchar(100) NOT NULL,
   `cat` varchar(100) DEFAULT NULL,
   `cet` varchar(100) DEFAULT NULL,
   `gate` varchar(100) DEFAULT NULL,
@@ -119,15 +99,17 @@ CREATE TABLE IF NOT EXISTS `futureplans` (
   `tofel` varchar(100) DEFAULT NULL,
   `others` varchar(100) DEFAULT NULL,
   `aos` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`std_rollno`)
+  PRIMARY KEY (`rollno`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `futureplans`
 --
 
-INSERT INTO `futureplans` (`std_rollno`, `cat`, `cet`, `gate`, `gre`, `tofel`, `others`, `aos`) VALUES
-('1', 'yes', 'yes', 'no', 'yes', 'yes', 'nothing', '');
+INSERT INTO `futureplans` (`rollno`, `cat`, `cet`, `gate`, `gre`, `tofel`, `others`, `aos`) VALUES
+('1', 'No', 'Yes', 'No', 'Yes', 'Yes', 'nothing', ''),
+('10', 'Yes', 'No', 'No', 'Yes', 'No', '', ''),
+('2', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -175,6 +157,17 @@ CREATE TABLE IF NOT EXISTS `qualidetail` (
   `sem8date` date NOT NULL DEFAULT current_timestamp(),
   `sem8sub` varchar(100) DEFAULT NULL,
   `sem8per` varchar(100) DEFAULT NULL,
+  `ssc_cert` varchar(200) DEFAULT NULL,
+  `dip_cert` varchar(200) DEFAULT NULL,
+  `dip_leave_cert` varchar(200) DEFAULT NULL,
+  `sem1_cert` varchar(200) DEFAULT NULL,
+  `sem2_cert` varchar(200) DEFAULT NULL,
+  `sem3_cert` varchar(200) DEFAULT NULL,
+  `sem4_cert` varchar(200) DEFAULT NULL,
+  `sem5_cert` varchar(200) DEFAULT NULL,
+  `sem6_cert` varchar(200) DEFAULT NULL,
+  `sem7_cert` varchar(200) DEFAULT NULL,
+  `sem8_cert` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`std_rollno`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -182,10 +175,10 @@ CREATE TABLE IF NOT EXISTS `qualidetail` (
 -- Dumping data for table `qualidetail`
 --
 
-INSERT INTO `qualidetail` (`std_rollno`, `sscmarks`, `sscdate`, `sscboard`, `dipmarks`, `dipdate`, `sem1attd`, `sem1date`, `sem1sub`, `sem1per`, `sem2attd`, `sem2date`, `sem2sub`, `sem2per`, `sem3attd`, `sem3date`, `sem3sub`, `sem3per`, `sem4attd`, `sem4date`, `sem4sub`, `sem4per`, `sem5attd`, `sem5date`, `sem5sub`, `sem5per`, `sem6attd`, `sem6date`, `sem6sub`, `sem6per`, `sem7attd`, `sem7date`, `sem7sub`, `sem7per`, `sem8attd`, `sem8date`, `sem8sub`, `sem8per`) VALUES
-('1', '100', '2020-09-04', 'SSC', '85', '2020-09-19', '78', '2020-09-02', 'abc', '99', '75', '2020-09-09', 'abc', '75', '75', '2010-12-12', 'abc', '87', '75', '2100-02-01', 'abc', '85', '100', '2010-10-26', 'qq', '83', 'wwe', '2020-09-26', 'www', '86', 'aaa', '2020-09-03', 'kn', '90', 'kjaskan', '2020-09-24', 'knknsk', '100'),
-('2', '2', '2020-10-10', 'CBSC', '100', '2020-10-10', '70', '2020-10-10', 'ss', '75', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', ''),
-('', NULL, '2020-10-10', NULL, NULL, '2020-10-10', NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL, NULL, '2020-10-10', NULL, NULL);
+INSERT INTO `qualidetail` (`std_rollno`, `sscmarks`, `sscdate`, `sscboard`, `dipmarks`, `dipdate`, `sem1attd`, `sem1date`, `sem1sub`, `sem1per`, `sem2attd`, `sem2date`, `sem2sub`, `sem2per`, `sem3attd`, `sem3date`, `sem3sub`, `sem3per`, `sem4attd`, `sem4date`, `sem4sub`, `sem4per`, `sem5attd`, `sem5date`, `sem5sub`, `sem5per`, `sem6attd`, `sem6date`, `sem6sub`, `sem6per`, `sem7attd`, `sem7date`, `sem7sub`, `sem7per`, `sem8attd`, `sem8date`, `sem8sub`, `sem8per`, `ssc_cert`, `dip_cert`, `dip_leave_cert`, `sem1_cert`, `sem2_cert`, `sem3_cert`, `sem4_cert`, `sem5_cert`, `sem6_cert`, `sem7_cert`, `sem8_cert`) VALUES
+('1', '100', '2020-09-04', 'SSC', '85', '2020-09-19', '78', '2020-09-02', 'abc', '99', '75', '2020-09-09', 'abc', '75', '75', '2010-12-12', 'abc', '87', '75', '2100-02-01', 'abc', '85', '100', '2010-10-26', 'qq', '83', 'wwe', '2020-09-26', 'www', '86', 'aaa', '2020-09-03', 'kn', '90', 'kjaskan', '2020-09-24', 'knknsk', '100', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('2', '2', '2020-10-10', 'CBSC', '100', '2020-10-10', '70', '2020-10-10', 'ss', '75', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', 'ss', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '2020-10-10', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+('10', NULL, '2020-10-13', NULL, NULL, '2020-10-13', NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, '2020-10-13', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -208,6 +201,7 @@ CREATE TABLE IF NOT EXISTS `register` (
 
 INSERT INTO `register` (`std_rollno`, `std_name`, `std_email`, `std_pass`) VALUES
 ('1', 'hardik', 'a@gmail.com', '123'),
+('10', 'Rohit', 'rohit@gmail.com', '123'),
 ('2', 'pd', 'pd@gmail.com', '123');
 
 -- --------------------------------------------------------
@@ -239,6 +233,14 @@ CREATE TABLE IF NOT EXISTS `studentinfo` (
   `sport2` varchar(100) DEFAULT NULL,
   `sport3` varchar(100) DEFAULT NULL,
   `health` varchar(100) DEFAULT NULL,
+  `idcard` varchar(200) DEFAULT NULL,
+  `doc_cert` varchar(200) DEFAULT NULL,
+  `hob1_cert` varchar(200) DEFAULT NULL,
+  `hob2_cert` varchar(200) DEFAULT NULL,
+  `hob3_cert` varchar(200) DEFAULT NULL,
+  `sport1_cert` varchar(200) DEFAULT NULL,
+  `sport2_cert` varchar(200) DEFAULT NULL,
+  `sport3_cert` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`rollno`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -246,10 +248,10 @@ CREATE TABLE IF NOT EXISTS `studentinfo` (
 -- Dumping data for table `studentinfo`
 --
 
-INSERT INTO `studentinfo` (`rollno`, `name`, `m_name`, `l_name`, `mobile`, `dept`, `currentyear`, `currentsem`, `address`, `bg`, `email`, `birthdate`, `monthaddm`, `year`, `hobbies`, `hobbie2`, `hobbie3`, `sports`, `sport2`, `sport3`, `health`) VALUES
-('1', 'Hardik', 'abc', 'Yewale', '10101010', 'Information Technology', 'First', 'SEM 1', 'Barcelona', 'a', 'hardik@gmail.com', '2020-10-01', '2010-10-10', 'First', '', 'Football', '', '', 'chess', 'volleyball', ''),
-('2', 'pd', 'sdd', 'hero', '12343212', 'Mechanical Engineering', 'First', 'SEM 1', 'Amsterdam', 'o', 'pd@gmail.com', '1992-10-01', '2020-10-07', 'First', '', '', '', '', '', '', 'handicapped'),
-('', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-10', '2020-10-10', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `studentinfo` (`rollno`, `name`, `m_name`, `l_name`, `mobile`, `dept`, `currentyear`, `currentsem`, `address`, `bg`, `email`, `birthdate`, `monthaddm`, `year`, `hobbies`, `hobbie2`, `hobbie3`, `sports`, `sport2`, `sport3`, `health`, `idcard`, `doc_cert`, `hob1_cert`, `hob2_cert`, `hob3_cert`, `sport1_cert`, `sport2_cert`, `sport3_cert`) VALUES
+('1', 'Hardik', 'abcc', 'Yewale', '10101010', 'Information Technology', 'First', 'SEM 1', 'Barcelona', 'a', 'hardik@gmail.com', '2020-10-01', '2010-10-10', 'First', 'cricket', 'Football', '', '', 'chess', 'volleyball', '', '', '', '', '', '', '', '', ''),
+('2', 'pd', 'sdd', 'hero', '12343212', 'Mechanical Engineering', 'First', 'SEM 1', 'Amsterdam', 'o', 'pd@gmail.com', '1992-10-01', '2020-10-07', 'First', '', '', '', 'cricket', 'chess', '', 'handicapped', '', '', '', '', NULL, '', NULL, NULL),
+('10', 'Rohit', 'abcc', 'abc', '1234567898', 'Information Technology', 'First', 'SEM 1', 'Munich', 'o', 'rohit@gmail.com', '2020-10-01', '2020-10-01', 'First', '', '', '', 'cricket', 'chess', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -264,15 +266,15 @@ CREATE TABLE IF NOT EXISTS `teacher` (
   `t_email` varchar(20) NOT NULL,
   `t_pass` varchar(20) NOT NULL,
   PRIMARY KEY (`tc_id`,`t_email`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `teacher`
 --
 
 INSERT INTO `teacher` (`tc_id`, `t_name`, `t_email`, `t_pass`) VALUES
-(3, 'teacher', 'teacher@gmai.com', '123'),
-(4, 'teacher2', 'teacher2@gmai.com', '123');
+(6, 'teacher2', 't2@gmail.com', '123'),
+(5, 'teacher1', 't1@gmail.com', '123');
 
 -- --------------------------------------------------------
 
